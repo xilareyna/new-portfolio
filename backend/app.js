22da1c9a114d9448c7c8add6c746b3412c1dbc46
xilareyna <68404722+xilareyna@users.noreply.github.com>
2021-03-01 15:27:21 -0600

    built backend server

---
+++ backend/app.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const mongoose = require("mongoose");
+const MONGOURI = process.env.MONGODB_URI;
+
+const formController = require("./controllers/formController.js");
+
+//============
+//Middleware
+//============
+app.use(express.json());
+
+//=====================
+//Database Disconnection
+//=====================
+mongoose.connection.on("error", (err) =>
+  console.log(err.message + " is Mongod not running?")
+);
+mongoose.connection.on("disconnected", () => console.log("mongo disconnected"));
+
+//=====================
+//Database Connection
+//=====================
+mongoose.connect(MONGOURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: true,
+});
+mongoose.connection.once("open", () => {
+  console.log("connected to mongoose");
+});
+
+//============
+//Listen Port
+//============
+app.listen(PORT, () => {
+  console.log(
+    "🎉🎊",
+    "getting organized with the Ticking Clock on port",
+    PORT,
+    "🎉🎊"
+  );
+});
+
+module.exports = app;
