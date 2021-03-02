d66142d216622566279861d4aeb4d49aca385288
xilareyna <68404722+xilareyna@users.noreply.github.com>
2021-03-01 18:36:33 -0600

    added controller to backend

---
+++ backend/controllers/formController.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const form = express.Router();
+const Form = require("../models/projectForm.js");
+
+//==============
+// Create Route
+//=============
+
+form.post("/", async (req, res) => {
+  try {
+    const createdForm = await Form.create(req.body.form);
+    res.status(200).json(updatedForm);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+//==============
+// Index Route
+//=============
+
+form.get("/", async (req, res) => {
+  try {
+    const foundForm = await Form.find({});
+    res.status(200).json(foundForm);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+//==============
+// Delete Route
+//=============
+
+form.delete("/:id", async (req, res) => {
+  try {
+    const deletedForm = await Form.findByIdAndRemove(req.params.id);
+    res.status(200).json(deletedForm);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+//==============
+// Update Route
+//=============
+
+form.put("/:id", async (req, res) => {
+  try {
+    const updatedForm = await Form.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json(updatedForm);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+module.exports = form;
